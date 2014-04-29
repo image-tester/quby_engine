49723bb702c71c994ed8ab28b0d32dae83976ffa
Marten Veldthuis <marten@veldthuis.com>
2014-04-29 14:39:12 +0200

    Print the name of the questionnaire in the stacktrace

--- app/dsl/quby/questionnaire_dsl.rb
+++ app/dsl/quby/questionnaire_dsl.rb
@@ -8,7 +8,7 @@ module Quby
   module QuestionnaireDsl
     def self.enhance(target_instance, definition)
       q = QuestionnaireBuilder.new(target_instance)
-      q.instance_eval(definition)
+      q.instance_eval(definition, target_instance.key.to_s)
     end
   end
 end
